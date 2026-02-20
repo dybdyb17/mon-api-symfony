86c435876e6342ffd7b11090482a94af559c51e5
Boudiaf Dybril <dybrilboudiaf14@gmail.com>
2026-02-20 15:30:15 +0100

    final

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,18 +5,23 @@ namespace App\Controller;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 final class PostController extends AbstractController
 {
-    #[Route('/api/post', name: 'api_post_index', methods: ['GET'])]
+    #[Route('/api/post', name: 'app_post_index', methods: ['GET'])]
     public function index(PostRepository $postRepository, SerializerInterface $serializer): JsonResponse
     {
         $posts = $postRepository->findAll();
 
         return $this->json([
-            'posts' => $serializer->normalize($posts, 'json')
-        ]);
+            'posts' => $serializer->normalize($posts),
+        ],
+            Response::HTTP_OK
+        );
     }
 }
+
